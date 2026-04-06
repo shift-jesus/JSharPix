cfe28ed57a3ef7b08af0bfcf52954604eadcb487
Jesus <jcampoy21@campusuninunez.edu.co>
2026-04-06 16:10:08 -0500

    Vercel add V2

--- backend/api/index.py
+++ backend/api/index.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.run import app
+
+handler = app
